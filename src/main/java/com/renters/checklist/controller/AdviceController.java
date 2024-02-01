c587965516f62af2d5b9c45b27d9966cf9670308
JC <jcsmileyjr@gmail.com>
2024-02-01 05:22:05 -0600

    Feature: Update advice's likes count in database

--- src/main/java/com/renters/checklist/controller/AdviceController.java
+++ src/main/java/com/renters/checklist/controller/AdviceController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renters.checklist.data.Advice;
@@ -16,13 +18,18 @@ public class AdviceController {
 	AdviceService adviceService;
 	
 	@GetMapping("/advice")
-	public String TestAdviceMessage() {
+	public String TestAdviceMessage () {
 		return "Hello World - Advice Controller";
 	}
 	
 	@GetMapping("/getAdvice")
-	public Iterable<Advice> getAdvice(){
+	public Iterable<Advice> getAdvice (){
 		return adviceService.getAllAdvice();
+	}
+	
+	@PutMapping("/updateLikes")
+	public void updatelikes (@RequestBody Advice advice) {
+		adviceService.updateLikes(advice, advice.getId());
 	}
 
 }
--- src/main/java/com/renters/checklist/service/AdviceService.java
+++ src/main/java/com/renters/checklist/service/AdviceService.java
@@ -1,6 +1,7 @@
 package com.renters.checklist.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,17 @@ public class AdviceService {
 	
 	public Iterable<Advice> getAllAdvice() {
 		return adviceRepository.findAll();
+	}
+	
+	public void updateLikes (Advice currentAdvice, Long id) {
+//		adviceRepository.deleteById(id);
+//		Advice newAdvice = new Advice();
+//		newAdvice.setAdvice(currentAdvice.getAdvice());
+//		newAdvice.setLikes(currentAdvice.getLikes() + 1);
+//		adviceRepository.save(newAdvice);
+		Advice newAdvice = adviceRepository.findById(id).get();
+		newAdvice.setLikes(currentAdvice.getLikes() + 1);
+		adviceRepository.save(newAdvice);
 	}
 
 }
